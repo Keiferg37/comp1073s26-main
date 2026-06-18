7efaccdd19f741fe9761a34734471fa60d5342f9
Keifer Grainger <200488755@student.georgianc.on.ca>
2026-06-17 21:30:31 -0400

    add classicSet array for sentence parts in script.js

---
+++ assignment-1/js/script.js
@@ -0,0 +1,8 @@
+// original words from the toy, one array per sentence part
+const classicSet = [
+    ["The turkey", "Mom", "The dog", "My teacher", "The elephant", "The cat"],
+    ["sat on", "ate", "danced with", "saw", "doesn't like", "kissed"],
+    ["a funny", "a scary", "a goofy", "a slimy", "a barking", "a fat"],
+    ["goat", "monkey", "fish", "cow", "frog", "bug", "worm"],
+    ["on the moon", "on the chair", "in my spagetti", "in my soup", "on the grass", "in my shoes"]
+];
